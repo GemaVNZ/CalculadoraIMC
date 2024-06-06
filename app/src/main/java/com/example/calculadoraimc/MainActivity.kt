1466618af3e2dc2a6e8db6c9ad98781471a9945d
GemaVNZ <gemavnz@gmail.com>
2024-06-06 10:40:46 +0200

    Calculadora IMC 1.0

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.calculadoraimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import org.w3c.dom.Text
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-     //lateinit var welcomeTextView: TextView
-     //lateinit var clickButton: Button
-     //lateinit var imageView: ImageView
+    lateinit var heightEditText: EditText
+    lateinit var weightTextView: TextView
+    lateinit var descriptionTextView: TextView
+    lateinit var resultTextView: TextView
+    lateinit var minusButton: Button
+    lateinit var manusButton: Button
+    lateinit var calculateButton: Button
+
+    var height:Int = 150
+    var weight:Int = 80
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,23 +30,45 @@ class MainActivity : AppCompatActivity() {
         //Se va a la vista del componente para buscar el id que le hemos indicado. Buscamos una referencia al componente de la vista. Ya podríamos programarlo.
         //Solamente damos ID cuando queramos programarlo.
 
-        //welcomeTextView = findViewById(R.id.welcomeTextView)
+        heightEditText = findViewById(R.id.heightEditText)
+        weightTextView = findViewById(R.id.weightTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
+        resultTextView = findViewById(R.id.descriptionTextView)
+        minusButton = findViewById(R.id.minusButton)
+        manusButton = findViewById(R.id.manusButton)
+        calculateButton = findViewById(R.id.calculateButton)
 
-        //Para cuando tengas el identificador en el string de cara al idioma
-        //welcome.TextView.text=getString(R.string.main_activity_welcome_text)
-
-        //welcomeTextView.text="Bienvenidos a mi primera aplicación Android."
+        setHeight()
+        setWeight()
 
 
-        //imageView = findViewById(R.id.imageView)
-        //imageView.setImageResource(R.drawable.ic_launcher_foreground)
+        minusButton.setOnClickListener {
+            weight--
+            setWeight()
+        }
 
 
-        //clickButton = findViewById(R.id.clickButton)
-        //clickButton.setOnClickListener {
-           //welcomeTextView.text="He hecho click en el botón"}
+        manusButton.setOnClickListener {
+            weight++
+            setWeight()
+
+        }
+
+        calculateButton.setOnClickListener {
+            height = heightEditText.text.toString().toInt()
+            val result = weight / (height/100f).pow(2)
+            resultTextView.text= result.toString()
+
+        }
+
     }
 
+        fun setHeight() {
+            heightEditText.setText(height.toString())
+        }
 
+        fun setWeight() {
+            weightTextView.text="$weight KG"
+        }
 
-}
+    }
